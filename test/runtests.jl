a646dfd85c29a7fdbaf5fa5a04036afa98e6b71c
Carl Morris <carl.morris.world@outlook.com>
2019-12-03 07:06:51 +0800

    Add basic tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,22 +8,52 @@ const filenames = [
     "Manifest.toml",
     "Project.toml",
     "README.md",
-    "scratch.jl",
     "src",
     "test",
 ]
 
-@testset "FnMatch.jl" begin
-    @testset "Ranges open left" begin
-        expected = [".git", ".gitignore", "LICENSE", "Manifest.toml", "Project.toml"]
-        pat = "[--P]*"
-        for n in filenames
-            if n in expected
-                @test fnmatchcase(n, pat)
-            else
-                @test !fnmatchcase(n, pat)
-            end
+function test_filter_match(pat, expected, reference)
+    for n in reference
+        if n in expected
+            @test fnmatchcase(n, pat)
+        else
+            @test !fnmatchcase(n, pat)
         end
-        @test all(fnfilter(filenames, pat) .== expected)
+    end
+    @test all(fnfilter(reference, pat) .== expected)
+end
+
+@testset "FnMatch.jl" begin
+    @testset "single range" begin
+        pat = "[--P]*"
+        expected = [".git", ".gitignore", "LICENSE", "Manifest.toml", "Project.toml"]
+        test_filter_match(pat, expected, filenames)
     end  # Ranges open left
+
+    @testset "multiple ranges" begin
+        pat = "[L-Ns-t]*"
+        expected = ["LICENSE", "Manifest.toml", "src", "test"]
+        test_filter_match(pat, expected, filenames)
+    end
+
+    @testset "? wilcard" begin
+        source = ["x1", "x2", "x", "xyz"]
+        pat = "x?"
+        expected = ["x1", "x2"]
+        test_filter_match(pat, expected, source)
+    end
+
+    @testset "* wildcard" begin
+        source = ["x12", "x22z", "abc", "xyz"]
+        pat = "x*z"
+        expected = ["x22z", "xyz"]
+        test_filter_match(pat, expected, source)
+    end
+
+    @testset "negated character set" begin
+        pat = "[!LMP]*"
+        expected = [".git", ".gitignore", "README.md", "src", "test"]
+        test_filter_match(pat, expected, filenames)
+    end
+
 end  # FnMatch
